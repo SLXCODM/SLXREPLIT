c036be9dc194da6006e250de7a48215f42579cca
SLXCODM <m1n3bas3@gmail.com>
2026-04-02 21:01:22 -0300

    chore: clean up redundant files and scripts

--- _BACKUP_COMMUNITY/client/src/community/lib/trpc.ts
+++
@@ -1,5 +0,0 @@
-import { createTRPCReact } from "@trpc/react-query";
-import type { AppRouter } from "../../../../server/community/appRouter";
-
-export const trpc = createTRPCReact<AppRouter>();
-export const api = trpc;
